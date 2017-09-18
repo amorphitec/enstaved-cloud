8846e850ecba5d7a31baf77d6ccf152a924842b8
James Stewart <jstewart101@gmail.com>
2017-09-18 13:36:13 +1000

    Setup hstore for shop and devshop dbs

--- postgres/config/init-shop-db.sh
+++ postgres/config/init-shop-db.sh
@@ -7,5 +7,6 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     \set password `echo $SHOP_DB_PASSWORD`
     CREATE USER :user WITH PASSWORD ':password';
     CREATE DATABASE :db;
+	CREATE EXTENSION IF NOT EXISTS hstore;
     GRANT ALL PRIVILEGES ON DATABASE :db TO :user;
 EOSQL
