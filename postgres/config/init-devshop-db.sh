d447e8bf57bf3af0342c8812db3b6c58bdc3c6da
James Stewart <james@amorphitec.io>
2019-05-06 21:27:35 +1000

    Revert db creation

--- postgres/config/init-devshop-db.sh
+++ postgres/config/init-devshop-db.sh
@@ -6,8 +6,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     \set user `echo $DEVSHOP_DB_USER`
     \set password `echo $DEVSHOP_DB_PASSWORD`
     CREATE USER :user WITH PASSWORD ':password';
-    SELECT 'CREATE DATABASE :db'
-    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = ':db')\gexec
+    CREATE DATABASE :db;
 	CREATE EXTENSION IF NOT EXISTS hstore;
     GRANT ALL PRIVILEGES ON DATABASE :db TO :user;
 EOSQL
--- postgres/config/init-shop-db.sh
+++ postgres/config/init-shop-db.sh
@@ -6,8 +6,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     \set user `echo $SHOP_DB_USER`
     \set password `echo $SHOP_DB_PASSWORD`
     CREATE USER :user WITH PASSWORD ':password';
-    SELECT 'CREATE DATABASE :db'
-    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = ':db')\gexec
+    CREATE DATABASE :db;
 	CREATE EXTENSION IF NOT EXISTS hstore;
     GRANT ALL PRIVILEGES ON DATABASE :db TO :user;
 EOSQL
